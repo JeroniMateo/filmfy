14cdd41ce6146cffec7fa7cee066449ebc83d739
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-11 15:48:53 +0200

    Install Bootstrap

---
+++ filmfy_vue_front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid ">
+    <h1 class="text-center text-muted">Vue 3 + Bootstrap 5</h1>
+  </div>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
