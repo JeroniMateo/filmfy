274a45573f4435923fc008819f890dcad2e968fe
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-11 17:37:00 +0200

    Top popular content cards

--- filmfy_vue_front/src/components/Welcome/FeaturedContent.vue
+++ filmfy_vue_front/src/components/Welcome/FeaturedContent.vue
@@ -1,79 +1,106 @@
 <template>
   <div class="container">
-    <h2>Main Content</h2>
+    <h4><b>Top Popular Content</b></h4>
   </div>
   <div class="container-fluid">
     <div class="row row-cols-1 row-cols-md-6 g-6">
-  <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Movies</small>
+          </div>
+        </div>
       </div>
-      <div class="card-footer">
-        <small class="text-muted">Movies</small>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Movies</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Series</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Series</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Documentaries</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100 w-70">
+          <img class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Documentaries</small>
+          </div>
+        </div>
       </div>
     </div>
-  </div> <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Movies</small>
-      </div>
-    </div>
-  </div> <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Series</small>
-      </div>
-    </div>
-  </div> <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Series</small>
-      </div>
-    </div>
-  </div> <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Documentaries</small>
-      </div>
-    </div>
-  </div> <div class="col">
-    <div class="card h-100 w-70">
-      <img  class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div class="card-footer">
-        <small class="text-muted">Documentaries</small>
-      </div>
-    </div>
-  </div>
-
-  
-</div>
   </div>
 </template>
 
