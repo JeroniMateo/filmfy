a58289994f6010d98327754b7d7759bbe332d65c
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-12 14:56:17 +0200

    Register Form

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="register">
-      
+    <form action="">
+      <input
+        id="emailR"
+        placeholder="Email*"
+        class="registro"
+        type="email"
+        name="email"
+      />
+      <p id="errorEmail" class="error"></p>
+      <input
+        id="passwordR"
+        placeholder="Password*"
+        class="registro"
+        type="password"
+        name="password"
+      />
+      <p id="errorPassword" class="error"></p>
+      <button
+        type="button"
+        id="signUpButton"
+        class="button"
+        aria-label="Regístrate"
+      >
+        Regístrate
+      </button>
+      <div>¿Ya tienes una cuenta?</div>
+      <button id="loginButton" class="button" aria-label="Inicia sesión">
+        <router-link to="../../../views/user/Login.vue">Inicia sesión</router-link>
+      </button>
+      <div id="signUpAlert"></div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    name: "Register",
-}
+  name: "Register",
+};
 </script>
 
 <style>
-
 </style>
