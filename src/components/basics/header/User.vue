10a73cd96c7b8a2e76408e923817ff8b5e3a6765
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-12 14:54:07 +0200

    Login Form

--- src/components/basics/header/User.vue
+++ src/components/basics/header/User.vue
@@ -1,52 +1,58 @@
 <template>
-    <div onclick="login()" class="user">
-      <!--If the user is not log -->
-      <div id="UserNotLog" class="btn" onclick="">
-        <img id="UserNotLogIMG" src="../../../assets/img/UserPhoto.png" alt=""  />
-        <p id="userProfile"></p>
-      </div>
-      <!--If the user is log -->
-      <!--Inside this div will show the User Name when it's loged -->
-      <div v-if="log" id="UserNotLog" hidden class="btn">
-        <div class="userAcount" >
-          <img src="../../../assets/img/UserPhoto.png" alt="" />
-          <li id="userProfile" class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <!--Username log-->
-              Username
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Perfil</a></li>
-              <li><a class="dropdown-item" href="#">Ajustes</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li>
-                <a class="dropdown-item" href="#">Log Out</a>
-              </li>
-            </ul>
-          </li>
-        </div>
+  <div onclick="login()" class="user">
+    <!--If the user is not log -->
+    <div id="UserNotLog" class="btn" onclick="">
+      <router-link to="../../../views/user/Login.vue">
+        <img
+          id="UserNotLogIMG"
+          src="../../../assets/img/UserPhoto.png"
+          alt=""
+        />
+        <p id="userProfile">Login</p>
+      </router-link>
+    </div>
+    <!--If the user is log -->
+    <!--Inside this div will show the User Name when it's loged -->
+    <div v-if="log" id="UserNotLog" hidden class="btn">
+      <div class="userAcount">
+        <img src="../../../assets/img/UserPhoto.png" alt="" />
+        <li id="userProfile" class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <!--Username log-->
+            Username
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Perfil</a></li>
+            <li><a class="dropdown-item" href="#">Ajustes</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li>
+              <a class="dropdown-item" href="#">Log Out</a>
+            </li>
+          </ul>
+        </li>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "User",
+  name: "User",
 };
 </script>
 
 <style>
-img#UserNotLogIMG{
+img#UserNotLogIMG {
   width: 5vh;
 }
-div.user{
+div.user {
   display: flex;
 }
 </style>
