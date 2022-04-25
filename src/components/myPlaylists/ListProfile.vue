79efb5dd9125035a0b947087ec08c75f546ee816
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-25 17:00:19 +0200

    Playlist of User favorite content

--- src/components/myPlaylists/ListProfile.vue
+++ src/components/myPlaylists/ListProfile.vue
@@ -4,17 +4,13 @@
       <div class="row row-cols-1 row-cols-md-3 g-4">
         <div class="col">
           <div class="card h-100 w-50">
-            <img  class="card-img-top" alt="..." />
+            <img class="card-img-top" alt="..." />
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
+              <h5 class="card-title">Favorites</h5>
+              <p class="card-text">Playlist of your favorite content.</p>
             </div>
             <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
+              <small class="text-muted">by Filmfy</small>
             </div>
           </div>
         </div>
