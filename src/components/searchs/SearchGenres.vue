7d85c97fe95dbb8af3082b140ed74427ac355bea
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-26 15:43:30 +0200

    SearchGenres & SearchContent Componets searching

---
+++ src/components/searchs/SearchGenres.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="SearchGenres">
+      <div class="container-fluid">
+          <WatchItemCarrousel/>
+      </div>
+  </div>
+</template>
+
+<script>
+import WatchItemCarrousel from '../../components/Items/watchContentGenres/WatchContantCarrousel.vue'
+export default {
+    name: 'SearchGenres',
+    comments:{
+        WatchItemCarrousel
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Searching/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import SearchContent from '../../components/searchs/SearchContent.vue'
+import SearchGenres from '../../components/searchs/SearchGenres.vue'
+export default {
+    name: 'Search',
+    components:{
+        SearchGenres,
+        SearchContent
+    }
+}
+</script>
+
+<style>
+
+</style>
