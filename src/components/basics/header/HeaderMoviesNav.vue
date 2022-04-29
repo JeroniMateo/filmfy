367f10890a187094476180a492a5b14876ba0927
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-04-29 08:38:14 +0200

    Header Movies Nav(Peliculas,Cartelera, Trailers y destacados

---
+++ src/components/basics/header/HeaderMoviesNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <ul class="nav nav-pills nav-fill">
+      <li class="nav-item">
+        <router-link class="nav-link active" aria-current="page" to="#"
+          >Peliculas</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" aria-current="page" to="#"
+          >Cartelera</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" aria-current="page" to="#"
+          >Trailers</router-link
+        >
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" aria-current="page"
+          >Destacados</router-link
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderMoviesNav",
+};
+</script>
+
+<style>
+</style>
