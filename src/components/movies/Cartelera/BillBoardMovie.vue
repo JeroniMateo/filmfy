50b72a7d36a1718d95d5519a3227f81e3ac480ec
jeroni.mateo <jeroni.mateo@omniaccess.com>
2022-05-02 16:52:12 +0200

    Home Sections

--- src/components/movies/Cartelera/BillBoardMovie.vue
+++ src/components/movies/Cartelera/BillBoardMovie.vue
@@ -17,37 +17,6 @@
           </div>
         </div>
       </div>
-      <div class="col">
-        <div class="card h-100">
-          <img class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This card has supporting text below as a natural lead-in to
-              additional content.
-            </p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img  class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,37 @@
 <template>
   <div class="home container">
     <div class="HomeDestacados">
-      <h3>Destacados</h3>
-      <div class="row">
-        <div class="col-md-4">
+      <h3>Destacados</h3>     
           <WatchItemCard />
-        </div>
-        <div class="col-md-4">
-          <WatchItemCard />
-        </div>
-        <div class="col-md-4">
-          <WatchItemCard />
-        </div>
-      </div>
     </div>
     <div class="cartelera">
       <h3>Cartelera</h3>
-      <div class="row">
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-      </div>
+        <BillBoardMovie />
     </div>
     <div class="UpcomingMovies">
       <h3>Próximos Estrenos</h3>
-      <div class="row">
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-        <div class="col-md-4">BillBoardMovie</div>
-      </div>
+      <BillBoardMovie />
     </div>
   </div>
 </template>
 
 <script>
-import BillBoardMovie from '@/components/movies/Cartelera/BillBoardMovie.vue'
-import WatchItemCard from '@/components/Items/WatchItemCard.vue'
+import BillBoardMovie from "@/components/movies/Cartelera/BillBoardMovie.vue"
+import WatchItemCard from "@/components/Items/WatchItemCard.vue"
 export default {
   name: 'HomeView',
   components: {
     BillBoardMovie,
-    WatchItemCard
+    WatchItemCard,
+    
   }
 }
 </script>
+<style scoped>
+h3{
+  color: rgb(29, 168, 226);
+  background-color: rgb(15, 23, 30);
+  font-size: 6vh;
+  width: 100%;
+}
+</style>
