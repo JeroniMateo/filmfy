4908dcc59b7fe23ce94d1945986f712f26a58199
Jeroni <jmateo@cifpfbmoll.eu>
2022-05-07 11:39:57 +0200

    Welcome index

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Welcome from '../views/Welcome'
+import Welcome from '../views/Welcome.vue'
 import Home from '../views/Home.vue'
 
 import Movies from '../views/watchContent/Movies.vue'
