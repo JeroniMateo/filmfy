550a966ae200fca5fb1fd9baff0c39dc75bc67ed
Jeroni Mateo <jeroni.mateo@omniaccess.com>
2022-05-18 12:13:03 +0200

    Movie content data Items movie & user

--- src/views/watchContent/MovieContent.vue
+++ src/views/watchContent/MovieContent.vue
@@ -2,103 +2,105 @@
   <div id="WatchingMovie">
     <h1 id="Movie Title"></h1>
     <div class="container-fluid">
-      <div class="card mb-3">
-        <div class="row g-0" id="MovieCardInfo">
-          <div id="MovieContentImage" class="col-md-4">
-            <img
-              id="img_movie"
-              src="../../assets/img/MoviesIMG/el-padrino.jpg"
-              class="img-fluid rounded-start"
-              alt="..."
-            />
-          </div>
-          <div class="col-md-8">
-            <div class="MainInfo card-body">
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item" id="MovieContentTitle">
-                  <label for="">Titulo:</label>
-                  <p id="title_movie">{{ title_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentYear">
-                  <label for="">Fecha:</label>
-                  <p id="date_movie">{{ date_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentGenre">
-                  <label for="">Genero:</label>
-                  <p id="category_movie">{{ category_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentDirectors">
-                  <label for="">Directores:</label>
-                  <p id="directors_movie">{{ directors_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentActors">
-                  <label for="">Actores:</label>
-                  <p id="actors_movie">{{ actors_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentRuntime">
-                  <label for="">Valoracíon:</label>
-                  <p id="rating_movie">{{ rating_movie }}</p>
-                </li>
-                <li class="list-group-item" id="MovieContentRuntime">
-                  <label for="">Vista <button></button></label>
-                </li>
-                <li class="list-group-item" id="MovieContentRuntime">
-                  <label for="">Favoritos <button></button></label>
-                </li>
-              </ul>
+      <div id="movie">
+        <div class="card mb-3">
+          <div class="row g-0" id="MovieCardInfo">
+            <div id="MovieContentImage" class="col-md-4">
+              <img
+                id="img_movie"
+                src="../../assets/img/MoviesIMG/el-padrino.jpg"
+                class="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="MainInfo card-body">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item" id="MovieContentTitle">
+                    <label for="">Titulo:</label>
+                    <p id="title_movie">{{ title_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentYear">
+                    <label for="">Fecha:</label>
+                    <p id="date_movie">{{ date_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentGenre">
+                    <label for="">Genero:</label>
+                    <p id="category_movie">{{ category_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentDirectors">
+                    <label for="">Directores:</label>
+                    <p id="directors_movie">{{ directors_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentActors">
+                    <label for="">Actores:</label>
+                    <p id="actors_movie">{{ actors_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentRuntime">
+                    <label for="">Valoracíon:</label>
+                    <p id="rating_movie">{{ rating_movie }}</p>
+                  </li>
+                  <li class="list-group-item" id="MovieContentRuntime">
+                    <label for="">Vista <button></button></label>
+                  </li>
+                  <li class="list-group-item" id="MovieContentRuntime">
+                    <label for="">Favoritos <button></button></label>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div id="MovieContentSinopsis">
-        <div id="SinopsisTitle">
-          <h2 class="SinopsisHeader" id="sinopsis_movie">Sinopsis</h2>
+        <div id="MovieContentSinopsis">
+          <div id="SinopsisTitle">
+            <h2 class="SinopsisHeader" id="sinopsis_movie">Sinopsis</h2>
+          </div>
+          <p id="sinopsis">
+            {{ sinopsis_movie }}
+          </p>
         </div>
-        <p id="sinopsis">
-          {{ sinopsis_movie }}
-        </p>
-      </div>
-      <div id="MovieContentValoration">
-        <h2>Valoración:</h2>
-        <button class="Rating">Valorar</button>
-        <br />
-        <div class="valoracion">
-          <!-- Estrella 1 -->
-          <button>
-            <i class="fas fa-star"></i>
-          </button>
+        <div id="MovieContentValoration">
+          <h2>Valoración:</h2>
+          <button class="Rating">Valorar</button>
+          <br />
+          <div class="valoracion">
+            <!-- Estrella 1 -->
+            <button>
+              <i class="fas fa-star"></i>
+            </button>
 
-          <!-- Estrella 2 -->
-          <button>
-            <i class="fas fa-star"></i>
-          </button>
+            <!-- Estrella 2 -->
+            <button>
+              <i class="fas fa-star"></i>
+            </button>
 
-          <!-- Estrella 3 -->
-          <button>
-            <i class="fas fa-star"></i>
-          </button>
+            <!-- Estrella 3 -->
+            <button>
+              <i class="fas fa-star"></i>
+            </button>
 
-          <!-- Estrella 4 -->
-          <button>
-            <i class="fas fa-star"></i>
-          </button>
+            <!-- Estrella 4 -->
+            <button>
+              <i class="fas fa-star"></i>
+            </button>
 
-          <!-- Estrella 5 -->
-          <button>
-            <i class="fas fa-star"></i>
-          </button>
+            <!-- Estrella 5 -->
+            <button>
+              <i class="fas fa-star"></i>
+            </button>
+          </div>
         </div>
-      </div>
-      <!--Trailer play automaticaly-->
-      <div id="Trailer trailer_movie">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/{{ trailer_movie }}"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+        <!--Trailer play automaticaly-->
+        <div id="Trailer trailer_movie">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/{{ trailer_movie }}"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
       <div id="Opinions">
         <h2 class="OpnionTitle">Comentarios y Valoraciones</h2>
@@ -119,7 +121,7 @@
             <button id="btnComentar" @click="comentar">Comentar</button>
           </div>
           <div id="Comments" style="display: none">
-            <div id="UserArea">
+            <div id="UserArea user">
               <div class="img_user_area">
                 <img
                   id="img_user"
@@ -150,21 +152,25 @@ export default {
   components: {},
   data() {
     return {
-      img_movie: '',
-      title_movie: '',
-      date_movie: '',
-      category_movie: [],
-      directors_movie: [],
-      actors_movie: [],
-      rating_movie: '',
-      sinopsis_movie: '',
-      trailer_movie: '',
+      movie: {
+        img_movie: '',
+        title_movie: '',
+        date_movie: '',
+        category_movie: [],
+        directors_movie: [],
+        actors_movie: [],
+        rating_movie: '',
+        sinopsis_movie: '',
+        trailer_movie: ''
+      },
 
-      img_user: '',
-      username: '',
-      commentaries: [],
-      user_comment: '',
-      date_comment: ''
+      user: {
+        img_user: '',
+        username: '',
+        commentaries: [],
+        user_comment: '',
+        date_comment: ''
+      }
     }
   },
   methods: {
