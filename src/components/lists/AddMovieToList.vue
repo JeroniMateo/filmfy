db3fefaf8e932d69c8cbec58621574d0ae8b2aec
Jeroni Mateo <jeroni.mateo@omniaccess.com>
2022-05-31 17:56:16 +0200

    Add Movie to a List

--- src/components/lists/AddMovieToList.vue
+++ src/components/lists/AddMovieToList.vue
@@ -1,54 +1,30 @@
 <template>
-  <div id="EditProfile">
+  <div id="AddMovieToList">
     <div class="container">
-      <h1>Perfil de Usuario</h1>
+      <h1>Añadir una nueva pelicula a una Lista</h1>
       <form class="row g-3 needs-validation" novalidate>
-        <div class="col-md-4">
-          <label for="name" class="form-label">Nombre</label>
-          <input
-            type="text"
-            class="form-control"
-            id="validationCustom02"
-            value=""
-          />
+        <div class="col-md-12">
+          <label for="movie_name" class="form-label"
+            >Nombre de la pelicula</label
+          >
+          <input type="text" class="form-control" id="movie_name" value="" />
         </div>
-        <div class="col-md-4">
-          <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" id="password" required />
+        <div class="col-md-12">
+          <label for="list_name" class="form-label">Nombre de la Lista</label>
+          <input type="text" class="form-control" id="list_name" required />
         </div>
-
-        <div class="col-md-4">
-          <label for="username" class="form-label">Username</label>
-          <div class="input-group has-validation">
-            <span class="input-group-text" id="inputGroupPrepend">@</span>
-            <input type="text" class="form-control" id="username" value="" />
-          </div>
-        </div>
-        <div class="col-md-6">
-          <label for="email" class="form-label">Email</label>
-          <input type="email" class="form-control" id="email" value="" />
-        </div>
-        <div class="col-md-6">
-          <div class="form-check">
-            <label id="Checkbox" class="form-check-label" for="invalidCheck">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                value=""
-                id="invalidCheck"
-                required
-              />
-              <small> Agree to terms and conditions </small>
-            </label>
-            <div class="invalid-feedback">
-              You must agree before submitting.
-            </div>
-          </div>
+        <div class="col-md-12">
+          <label for="username" class="form-label">Nombre de usuario</label>
+          <input type="text" class="form-control" id="username" required />
         </div>
 
         <div class="col-12">
-          <button class="btn btn-primary" type="submit" @click="editProfile">
-            Guardar Cambios
+          <button
+            class="btn btn-primary"
+            type="submit"
+            @click="añadirNuevaPelicula"
+          >
+            Añadir Pelicula
           </button>
         </div>
       </form>
@@ -61,62 +37,25 @@ export default {
   name: 'EditProfile',
   data() {
     return {
-      user: {
-        name: '',
-        username: '',
-        email: '',
-        password: ''
-      }
+      list_name: '',
+      username: '',
+      movie_name: ''
     }
   },
-  beforeMount() {
-    this.getUserProfile()
-  },
   methods: {
-    getUserProfile (id) {
-      fetch(`http://filmfy-api.ddns.net/api/register/${id}`, {
+    añadirNuevaPelicula () {
+      fetch('http://filmfy-api.ddns.net/api/register/', {
         method: 'POST',
         body: JSON.stringify({
-          usernameR: this.username,
-          nameR: this.name,
-          emailR: this.email,
-          passwordR: this.password,
-          password_confirmR: this.password_confirm
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      })
-        .then((response) => response.json())
-        .then((token) => {
-          this.setCookie('tokenName', token.access_token, 365)
-          this.log = true
-          this.$router.push('/')
-        })
-        .catch(function (error) {
-          console.log('Error en el fetch', error)
-        })
-    },
-
-    editProfile (id) {
-      fetch(`http://filmfy-api.ddns.net/api/register/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          name: this.name,
-          password: this.password,
+          list_name: this.list_name,
           username: this.username,
-          email: this.email
+          movie_name: this.movie_name
         }),
         headers: {
           'Content-Type': 'application/json'
         }
       })
         .then((response) => response.json())
-        .then((token) => {
-          this.setCookie('tokenName', token.access_token, 365)
-          this.log = true
-          this.$router.push('/')
-        })
         .catch(function (error) {
           console.log('Error en el fetch', error)
         })
