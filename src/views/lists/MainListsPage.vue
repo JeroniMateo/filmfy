8bbf0c53d0a6e4e2fd985f35d97c324e136f73b5
ToniGar20 <tgarcia@habitissimo.com>
2022-06-01 19:14:40 +0200

    some fixes

--- src/views/lists/MainListsPage.vue
+++ src/views/lists/MainListsPage.vue
@@ -57,34 +57,26 @@
           </div>
 
           <div>
-            <div class="d-flex justify-content-start mt-4" v-for="list in listsRecent.slice(0,5)">
-              <div class="list d-flex row">
-                <div class="col-7">
-                  <a :href="baseURL + '/lists/' + list.l_id" style="text-decoration: none">
-                    <div class="image-overlap">
-                      <span class="movie-img" v-for="movie in list.movies.slice(0,5)" >
-                        <img :src="'http://filmfy-api.ddns.net' + movie.image" width="75" height="133" :alt="movie.title"/>
-                      </span>
-                    </div>
-                    <div class="list-data d-flex flex-row m-1">
-                      <img :src="'http://filmfy-api.ddns.net' + list.user.profile_image" :alt="list.user.name" width="20" height="20"/>
-                      <span class="mx-2">{{ list.user.name }}</span>
-                      <span class="mx-2">❤ {{ list.l_likes }}</span>
-                      <span> <i class="fa-solid fa-film"></i> {{ list.movies_count }}</span>
-                    </div>
-                  </a>
+            <div class="d-flex justify-content-center mt-4" v-for="list in listsRecent.slice(0,5)">
+              <a :href="baseURL + '/lists/' + list.l_id" style="text-decoration: none">
+                <div class="image-overlap">
+                  <span class="movie-img" v-for="movie in list.movies.slice(0,5)">
+                    <img :src="'http://filmfy-api.ddns.net' + movie.image" width="95" height="160" :alt="movie.title"/>
+                  </span>
                 </div>
+                <div class="list-details d-flex flex-column">
+                  <div class="list-title d-flex flex-row justify-content-start">
+                    <span style="text-align: left"><b>{{ list.l_title }}</b></span>
+                  </div>
+                  <div class="list-data d-flex flex-row">
+                    <img :src="'http://filmfy-api.ddns.net' + list.user.profile_image" :alt="list.user.name" width="20" height="20"/>
+                    <span class="mx-2">{{ list.user.name }}</span>
+                    <span class="mx-2">❤ {{ list.l_likes }}</span>
+                    <span> <i class="fa-solid fa-film"></i> {{ list.movies_count }}</span>
+                  </div>
 
-                <div class="list-details d-flex flex-column col-5">
-                  <div class="list-title d-flex flex-row justify-content-start m-1">
-                    <span style="text-align: left">{{ list.l_title }}</span>
-                  </div>
-                  <div class="list-description d-flex flex-row justify-content-start m-1">
-                    <span style="text-align: left">{{ list.l_description }}</span>
-                  </div>
                 </div>
-
-              </div>
+              </a>
             </div>
           </div>
 
