73f095dd6cc6dd5c5cee67c0f962c9253839fdf1
Jeroni <jmateo@cifpfbmoll.eu>
2022-06-28 19:04:49 +0200

    Crear debate de una Pelicula

--- src/components/movies/detailed-movie-page/AsideDetailedMovie.vue
+++ src/components/movies/detailed-movie-page/AsideDetailedMovie.vue
@@ -1,71 +1,91 @@
 <template>
-
-  <aside v-if="this.log" class="aside-card d-flex flex-column align-items-start rounded-3 p-0">
+  <aside
+    v-if="this.log"
+    class="aside-card d-flex flex-column align-items-start rounded-3 p-0"
+  >
     <div id="likes">
-      <div v-if="liked" class="m-auto p-3" >
+      <div v-if="liked" class="m-auto p-3">
         <i class="fa-solid fa-heart me-2 fs-3" style="color: orange"></i>
       </div>
-      <div v-else  @click="this.sendLike" class="m-auto p-3" >
-        <i class="fa-solid fa-heart me-2 heart" ></i>
+      <div v-else @click="this.sendLike" class="m-auto p-3">
+        <i class="fa-solid fa-heart me-2 heart"></i>
       </div>
     </div>
 
     <div class="text-center p-3">
-      <span @click="this.displayFormAddToList" class="addComment">Añadir a lista</span>
+      <span @click="this.displayFormAddToList" class="addComment"
+        >Añadir a lista</span
+      >
     </div>
 
     <div v-if="commented" class="text-center p-3">
-      <a class="text-decoration-none text-white commented"><span >Película comentada</span></a>
+      <a class="text-decoration-none text-white commented"
+        ><span>Película comentada</span></a
+      >
     </div>
     <div v-else class="text-center p-3">
-      <a @click="this.displayModalForm" class="text-decoration-none text-white addComment"><span >Añadir un comentario</span></a>
+      <a
+        @click="this.displayModalForm"
+        class="text-decoration-none text-white addComment"
+        ><span>Añadir un comentario</span></a
+      >
+    </div>
+    <div class="text-center p-3">
+      <span @click="this.displayFormCreateDabate" class="addComment"
+        >Crear Debate</span
+      >
     </div>
   </aside>
 
-  <aside v-else class="aside-card d-flex flex-column align-items-start rounded-3 p-0">
-
-    <router-link :to="{ name: 'login' }" class="text-decoration-none" style="cursor: pointer">
+  <aside
+    v-else
+    class="aside-card d-flex flex-column align-items-start rounded-3 p-0"
+  >
+    <router-link
+      :to="{ name: 'login' }"
+      class="text-decoration-none"
+      style="cursor: pointer"
+    >
       <div class="m-auto p-3">
-        <span class="text-center text-white">Unete a filmfy para poder añadir peliculas a tu listas y darle me gusta</span>
+        <span class="text-center text-white"
+          >Unete a filmfy para poder añadir peliculas a tu listas y darle me
+          gusta</span
+        >
       </div>
     </router-link>
-
   </aside>
 
-  <FormModal id="modal" :movie="this.movie" :user="user"/>
-  <FormAddMovieList id="formAddMovieToList" :movie="movie" :user="user"/>
-
+  <FormModal id="modal" :movie="this.movie" :user="user" />
+  <FormAddMovieList id="formAddMovieToList" :movie="movie" :user="user" />
 </template>
 
 <script>
-import FormModal from "@/components/movies/detailed-movie-page/FormModal";
-import {getCookie, getUser, origin} from "@/main.js"
-import FormAddMovieList from "@/components/movies/detailed-movie-page/FormAddMovieList";
-
+import FormModal from '@/components/movies/detailed-movie-page/FormModal'
+import { getCookie, getUser, origin } from '@/main.js'
+import FormAddMovieList from '@/components/movies/detailed-movie-page/FormAddMovieList'
 
 export default {
-  props: ["movie"],
-  name: "AsideDetailedMovie",
-  components: {FormAddMovieList, FormModal},
+  props: ['movie'],
+  name: 'AsideDetailedMovie',
+  components: { FormAddMovieList, FormModal },
 
   data() {
     return {
       baseUrl: origin(),
       log: false,
-      token: "",
-      user: "",
+      token: '',
+      user: '',
       liked: false,
       commented: false,
-      counter : 0,
+      counter: 0
     }
   },
 
   async beforeMount() {
-
-    this.token = getCookie("auth")
+    this.token = getCookie('auth')
     if (this.token && this.counter === 0) {
       this.user = await getUser(this.token)
-      if (this.user !== "User expired") {
+      if (this.user !== 'User expired') {
         this.log = true
         await this.checkLiked()
         await this.checkCommented()
@@ -75,38 +95,43 @@ export default {
   },
 
   methods: {
-
-    async checkLiked(){
-      let promise = await fetch("http://filmfy-api.ddns.net/api/user-had-like-movie", {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          "movie": this.movie.id,
-          "user": this.user.id
-        })
-      })
+    async checkLiked() {
+      let promise = await fetch(
+        'http://filmfy-api.ddns.net/api/user-had-like-movie',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            movie: this.movie.id,
+            user: this.user.id
+          })
+        }
+      )
 
       let response = await promise.json()
       if (response.status === 1) {
         this.liked = true
-      }else {
+      } else {
         this.liked = false
       }
     },
 
     async checkCommented() {
-      let promise = await fetch("http://filmfy-api.ddns.net/api/user-had-comment", {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          "movie": this.movie.id,
-          "user": this.user.id
-        })
-      })
+      let promise = await fetch(
+        'http://filmfy-api.ddns.net/api/user-had-comment',
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            movie: this.movie.id,
+            user: this.user.id
+          })
+        }
+      )
 
       let response = await promise.json()
 
@@ -118,44 +143,43 @@ export default {
     },
 
     async sendLike() {
-
-      await fetch("http://filmfy-api.ddns.net/api/v1/movies-likes", {
-        method: "POST",
+      await fetch('http://filmfy-api.ddns.net/api/v1/movies-likes', {
+        method: 'POST',
         headers: {
           'Content-type': 'application/json',
           'Access-Control-Allow-Origin': '*',
-          'Authorization': 'Bearer ' + this.token,
+          Authorization: 'Bearer ' + this.token
         },
         body: JSON.stringify({
-          "movies_id": this.$route.params.movie,
-          "users_id": this.user.id
+          movies_id: this.$route.params.movie,
+          users_id: this.user.id
         })
       })
 
       await location.reload()
-
     },
 
     displayModalForm() {
-      let formModal = document.getElementById("modal")
-      formModal.style.display = "block"
+      let formModal = document.getElementById('modal')
+      formModal.style.display = 'block'
     },
 
     displayFormAddToList() {
-      let formAddMovieToList = document.getElementById("formAddMovieToList")
-      formAddMovieToList.style.display = "block"
+      let formAddMovieToList = document.getElementById('formAddMovieToList')
+      formAddMovieToList.style.display = 'block'
+    },
+    displayFormCreateDabate() {
+      this.$router.push('/forum')
     }
   }
 }
 </script>
 
 <style scoped>
-
 .aside-card {
-  background-color: #2C3440;
+  background-color: #2c3440;
   width: fit-content;
 }
-
 
 aside > div {
   width: -webkit-fill-available;
@@ -165,6 +189,4 @@ aside > div {
 aside > div:last-child {
   border-bottom: none;
 }
-
-
 </style>
