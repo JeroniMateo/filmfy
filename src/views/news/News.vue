ead8fdf82c29c0a16b4b74bb36b0025e1ae6fc1c
Jeroni <jmateo@cifpfbmoll.eu>
2022-07-11 18:50:16 +0200

    NewsCarrousel

--- src/views/news/News.vue
+++ src/views/news/News.vue
@@ -1,13 +1,17 @@
 <template>
-  
+  <div id="News">
+      <div>
+          <!-- <NewsCarrousel /> -->
+    </div>
+  </div>
 </template>
 
 <script>
+import NewsCarrousel from '../../components/news/NewsCarrousel.vue'
 export default {
-
+  name: 'News',
+  components: { NewsCarrousel }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
